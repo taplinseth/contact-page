8f1243c2b7481f022eb0c55a3772d6a0cfa06e40
taplinseth <taplinseth@gmail.com>
2021-12-07 14:10:10 -0600

    added ability to store data on results.html

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-function mysubmit(event) {
-    event.preventDefault();
-    console.log("test");
-}
+// function mysubmit(event) {
+//     event.preventDefault();
+//     console.log("test");
+// }
+
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
